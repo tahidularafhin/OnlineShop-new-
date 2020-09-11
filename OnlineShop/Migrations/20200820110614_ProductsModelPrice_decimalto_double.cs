dd0454de38e85b807db4a25ac25d1227c8d90946
tahidularafhin <48841466+tahidularafhin@users.noreply.github.com>
2020-09-11 13:03:06 +0200

    first commit

---
+++ OnlineShop/Migrations/20200820110614_ProductsModelPrice_decimalto_double.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.Migrations
+{
+    public partial class ProductsModelPrice_decimalto_double : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
